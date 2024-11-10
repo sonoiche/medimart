5134836d9172f28eacf7e0c55f647d4d49ff0e72
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2024-11-10 15:52:19 +0800

    modify dashboard

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Client\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('login');
 });
 
 Auth::routes();
